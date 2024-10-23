c3efd73eaddd969a3b34fcdc0b62bff446528dfc
Canh Truong <canhtruong2412!@gmail.com>
2024-10-23 10:18:01 +0700

    CanhTT/Implement-Spot-Services

--- SpotService/Controllers/Atraction/AtractionController.cs
+++ SpotService/Controllers/Atraction/AtractionController.cs
@@ -30,5 +30,78 @@ namespace SpotService.Controllers.Atraction
 
             return Ok(query);
         }
+
+        [HttpGet("Keys")]
+        public ActionResult<AttractionKeyValueDTO> GetAttractionKeyValue()
+        {
+            var keyValuePairs = from attr in _context.AtrHeads.AsNoTracking()
+                                select new AttractionKeyValueDTO
+                                {
+                                    AtrKey = attr.AtrKey,   
+                                    Country = attr.Country
+                                };
+
+            return Ok(keyValuePairs);
+        }
+
+        [HttpGet]
+        public ActionResult<DAttractionDTO> GetAttractionDetail([FromQuery] Guid atrId)
+        {
+            if (string.IsNullOrEmpty(atrId.ToString()))
+            {
+                throw new Exception("Id is empty");
+            }
+
+            // Load the main attraction
+            var attraction = (from head in _context.AtrHeads.AsNoTracking()
+                              where head.AtrKey == atrId
+                              join continent in _context.Constants.AsNoTracking() on head.Contitnent equals continent.ConstantCode
+                              join sub in _context.Constants.AsNoTracking() on head.SubContitnent equals sub.ConstantCode
+                              select new DAttractionDTO
+                              {
+                                  AtrKey = head.AtrKey,
+                                  Contitnent = continent.ConstantName,
+                                  SubContitnent = sub.ConstantName,
+                                  Description = head.Description,
+                                  Country = head.Country,
+                                  PicKey = head.PicKey
+                              }).FirstOrDefault();
+
+            if (attraction == null)
+            {
+                return NotFound();
+            }
+
+            attraction.AtrContents = _context.AtrContents.AsNoTracking()
+                .Where(content => content.AtrKey == atrId)
+                .Select(content => new AtrContent
+                {
+                    ContentKey = content.ContentKey,
+                    Content = content.Content,
+                    Title = content.Title,
+                    ContentType = content.ContentType
+                }).ToList();
+
+            attraction.DesHeads = _context.DesHeads.AsNoTracking()
+                .Where(des => des.AtrKey == atrId)
+                .Select(des => new DesHead
+                {
+                    DesKey = des.DesKey,
+                    Description = des.Description,
+                    DesName = des.DesName
+                }).ToList();
+
+            attraction.PlcHeads = _context.PlcHeads.AsNoTracking()
+                .Where(plc => plc.AtrKey == atrId)
+                .Select(plc => new PlcHead
+                {
+                    PlcKey = plc.PlcKey,
+                    Description = plc.Description,
+                    PlcName = plc.PlcName
+                }).Take(6).ToList();
+
+            return attraction;
+        }
+
     }
 }
--- SpotService/Controllers/Atraction/AtractionDTO.cs
+++ SpotService/Controllers/Atraction/AtractionDTO.cs
@@ -1,4 +1,8 @@
-﻿namespace SpotService.Controllers.Atraction
+﻿using SpotService.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpotService.Controllers.Atraction
 {
     public class AtractionDTO
     {
@@ -7,5 +11,28 @@
         public string SubContinent { get; set; }
         public string Country { get; set; }
         public string PicLink { get; set; }
+    }
+
+    public class DAttractionDTO
+    {
+        public Guid AtrKey { get; set; }
+
+        public string? Description { get; set; }
+        public string? Contitnent { get; set; }
+        public string? SubContitnent { get; set; }
+        public string? Country { get; set; }
+
+        public Guid? PicKey { get; set; }
+        public virtual ICollection<AtrContent> AtrContents { get; set; } = new List<AtrContent>();
+
+        public virtual ICollection<DesHead> DesHeads { get; set; } = new List<DesHead>();
+
+        public virtual ICollection<PlcHead> PlcHeads { get; set; } = new List<PlcHead>();
+    }
+
+    public class AttractionKeyValueDTO
+    {
+        public Guid AtrKey { get; set; }
+        public string? Country { get; set; }
     }
 }
