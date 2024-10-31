5d552a57a65ea2aca26125b0a9c3b2781603014d
Canh Truong <canhtruong2412!@gmail.com>
2024-10-31 10:39:07 +0700

    SpotService-Init review

---
+++ SpotService/Model/EvlHead.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpotService.Model
+{
+    [Table("EVL_HEAD", Schema = "EVALUATE")]
+    [Index("EvlKey", Name = "EVL_PLC_index_1")]
+    public class EvlHead
+    {
+        [Key]
+        [Column("EVL_KEY")]
+        public Guid EvlKey { get; set; }
+        [Column("EVL_TITLE")]
+        [StringLength(100)]
+        public string EvlTitle { get; set; }
+        [Column("EVL_CONTENT")]
+        [StringLength(255)]
+        public string EvlContent { get; set; }
+        [Column("PIC_KEY")]
+        public Guid PicKey { get; set; }
+    }
+}
